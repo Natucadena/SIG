2832f3dc9f44a24615c1efe208a2cf1c2d8b411a
Ivone Romero <ivoneromero9@gmail.com>
2025-07-18 18:38:47 -0500

    Se creo el programa

---
+++ Miprimerprograma.java
@@ -0,0 +1,20 @@
+public class Miprimerprograma{
+       public static void main(String args[]){
+              int valor1=0;
+              String mensaje="Hola, mi nombre es Nata";
+              if(valor1>0){
+                       System.out.println("El valor es mayor a 0");
+                       }else{
+                             System.out.println("El valor es menor o igual a 0");
+              }
+              while(valor1<10){
+                      valor1=valor1+1;
+                      System.out.println(valor1);
+              }
+              for(int i=0;i<9;i++){
+                      System.out.println(i);
+              }
+
+       }
+}
+
---
+++ Miprimerprograma1.java
@@ -0,0 +1,13 @@
+public class Miprimerprograma1{
+       public static void sumar(int a, int b){
+               System.out.println(a+b);
+       }
+       public static int suma(int a, int b){
+               return a+b;
+       }
+       public static void main(String args[]){
+               sumar(5,7);
+               System.out.println(suma(6,6));
+       }
+}
+
